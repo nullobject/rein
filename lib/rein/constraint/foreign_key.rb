bf185111fc76528e020b9e40c8f9899edbff7312
Josh Bassett <josh.bassett@gmail.com>
2010-10-28 15:41:15 +1100

    [ADDED] Ability to remove a foreign key constraint.

--- lib/rein/constraint/foreign_key.rb
+++ lib/rein/constraint/foreign_key.rb
@@ -19,8 +19,24 @@ module RC
       # A foreign key constraint doesn't have an implicit index.
       add_index(referencing_table, referencing_attribute) if options[:add_index] == true
     end
-
     alias_method :add_foreign_key, :add_foreign_key_constraint
+
+    def remove_foreign_key_constraint(referencing_table, referenced_table, options = {})
+      referencing_attribute = options[:referencing] || "#{referenced_table.to_s.singularize}_id".to_sym
+      referenced_attribute  = "id".to_sym
+
+      name = options[:name] || "#{referencing_attribute}_fk".to_sym
+
+      if self.class.is_a?(ActiveRecord::ConnectionAdapters::MysqlAdapter)
+        execute "ALTER TABLE #{referencing_table} DROP FOREIGN KEY #{name}"
+      else
+        execute "ALTER TABLE #{referencing_table} DROP CONSTRAINT #{name}"
+      end
+
+      # A foreign key constraint doesn't have an implicit index.
+      remove_index(referencing_table, referencing_attribute) if options[:remove_index] == true
+    end
+    alias_method :remove_foreign_key, :remove_foreign_key_constraint
 
   private
     def referential_action(action)
--- spec/rein/constraint/foreign_key_spec.rb
+++ spec/rein/constraint/foreign_key_spec.rb
@@ -1,56 +1,92 @@
 require 'spec_helper'
 
-describe RC::ForeignKey, "#add_foreign_key_constraint" do
+describe RC::ForeignKey do
   let(:adapter) do
     Class.new do
       include RC::ForeignKey
     end.new
   end
 
-  subject { adapter }
+  describe "#add_foreign_key_constraint" do
+    subject { adapter }
 
-  before do
-    stub(adapter).execute
-    stub(adapter).add_index
+    before do
+      stub(adapter).execute
+      stub(adapter).add_index
+    end
+
+    context "with no options" do
+      before { adapter.add_foreign_key_constraint(:books, :people) }
+      it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE RESTRICT ON UPDATE RESTRICT") }
+    end
+
+    context "with a given referencing attribute" do
+      before { adapter.add_foreign_key_constraint(:books, :people, :referencing => :author_id) }
+      it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT author_id_fk FOREIGN KEY (author_id) REFERENCES people (id) ON DELETE RESTRICT ON UPDATE RESTRICT") }
+    end
+
+    context "with a given name" do
+      before { adapter.add_foreign_key_constraint(:books, :people, :name => :foo) }
+      it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT foo FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE RESTRICT ON UPDATE RESTRICT") }
+    end
+
+    context "with a given on delete referential action" do
+      before { adapter.add_foreign_key_constraint(:books, :people, :on_delete => :cascade) }
+      it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE RESTRICT") }
+    end
+
+    context "with a given on update referential action" do
+      before { adapter.add_foreign_key_constraint(:books, :people, :on_update => :cascade) }
+      it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE RESTRICT ON UPDATE CASCADE") }
+    end
+
+    context "with a given on delete and update referential action" do
+      before { adapter.add_foreign_key_constraint(:books, :people, :on_delete => :cascade, :on_update => :cascade) }
+      it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE") }
+    end
+
+    describe "with a given add_index option" do
+      before { adapter.add_foreign_key_constraint(:books, :people, :add_index => true) }
+      it { should have_received.add_index(:books, :person_id) }
+    end
+
+    describe "with a referencing attribute and a add_index option" do
+      before { adapter.add_foreign_key_constraint(:books, :people, :referencing => :author_id, :add_index => true) }
+      it { should have_received.add_index(:books, :author_id) }
+    end
   end
 
-  context "with no options" do
-    before { adapter.add_foreign_key_constraint(:books, :people) }
-    it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE RESTRICT ON UPDATE RESTRICT") }
-  end
+  describe "#remove_foreign_key_constraint" do
+    subject { adapter }
 
-  context "with a given referencing attribute" do
-    before { adapter.add_foreign_key_constraint(:books, :people, :referencing => :author_id) }
-    it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT author_id_fk FOREIGN KEY (author_id) REFERENCES people (id) ON DELETE RESTRICT ON UPDATE RESTRICT") }
-  end
+    before do
+      stub(adapter).execute
+      stub(adapter).remove_index
+    end
 
-  context "with a given name" do
-    before { adapter.add_foreign_key_constraint(:books, :people, :name => :foo) }
-    it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT foo FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE RESTRICT ON UPDATE RESTRICT") }
-  end
+    context "with no options" do
+      before { adapter.remove_foreign_key_constraint(:books, :people) }
+      it { should have_received.execute("ALTER TABLE books DROP CONSTRAINT person_id_fk") }
+    end
 
-  context "with a given on delete referential action" do
-    before { adapter.add_foreign_key_constraint(:books, :people, :on_delete => :cascade) }
-    it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE RESTRICT") }
-  end
+    context "with a given referencing attribute" do
+      before { adapter.remove_foreign_key_constraint(:books, :people, :referencing => :author_id) }
+      it { should have_received.execute("ALTER TABLE books DROP CONSTRAINT author_id_fk") }
+    end
 
-  context "with a given on update referential action" do
-    before { adapter.add_foreign_key_constraint(:books, :people, :on_update => :cascade) }
-    it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE RESTRICT ON UPDATE CASCADE") }
-  end
+    context "with a given name" do
+      before { adapter.remove_foreign_key_constraint(:books, :people, :name => :foo) }
+      it { should have_received.execute("ALTER TABLE books DROP CONSTRAINT foo") }
+    end
 
-  context "with a given on delete and update referential action" do
-    before { adapter.add_foreign_key_constraint(:books, :people, :on_delete => :cascade, :on_update => :cascade) }
-    it { should have_received.execute("ALTER TABLE books ADD CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE") }
-  end
+    describe "with a given remove_index option" do
+      before { adapter.remove_foreign_key_constraint(:books, :people, :remove_index => true) }
+      it { should have_received.remove_index(:books, :person_id) }
+    end
 
-  describe "with a given add_index option" do
-    before { adapter.add_foreign_key_constraint(:books, :people, :add_index => true) }
-    it { should have_received.add_index(:books, :person_id) }
-  end
-
-  describe "with a referencing attribute and a add_index option" do
-    before { adapter.add_foreign_key_constraint(:books, :people, :referencing => :author_id, :add_index => true) }
-    it { should have_received.add_index(:books, :author_id) }
+    describe "with a referencing attribute and a remove_index option" do
+      before { adapter.remove_foreign_key_constraint(:books, :people, :referencing => :author_id, :remove_index => true) }
+      it { should have_received.remove_index(:books, :author_id) }
+    end
   end
 end
