36681f3bde062dfef3a7defb4eabb3e6751e059a
Josh Bassett <josh.bassett@gmail.com>
2017-04-08 19:38:32 +1000

    Add constraints integration test

--- rein.gemspec
+++ rein.gemspec
@@ -16,9 +16,11 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency "activerecord", ">= 4.0.0", "< 6"
+  spec.add_runtime_dependency "activesupport", ">= 4.0.0", "< 6"
 
   spec.add_development_dependency "appraisal", "~> 2.1"
   spec.add_development_dependency "bundler", "~> 1.14"
+  spec.add_development_dependency "pg"
   spec.add_development_dependency "rake", "~> 12.0"
   spec.add_development_dependency "rspec", "~> 3.5"
   spec.add_development_dependency "rubocop", "~> 0.47"
---
+++ spec/integration/constraints_spec.rb
@@ -0,0 +1,55 @@
+require "spec_helper"
+
+class CreateBooksTable < ActiveRecord::Migration
+  def change
+    suppress_messages do
+      execute "DROP TABLE IF EXISTS books"
+
+      create_table :books do |t|
+        t.string :title, null: false
+        t.string :state, null: false
+        t.integer :published_month, null: false
+        t.date :due_date
+      end
+
+      add_presence_constraint :books, :title
+      add_inclusion_constraint :books, :state, in: %w[available on_loan on_hold]
+      add_numericality_constraint :books, :published_month, greater_than_or_equal_to: 1, less_than_or_equal_to: 12
+    end
+  end
+end
+
+class Book < ActiveRecord::Base; end
+
+def create_book(attributes = {})
+  attributes = {
+    title: "foo",
+    state: "available",
+    published_month: 1
+  }.update(attributes)
+
+  Book.create!(attributes)
+end
+
+RSpec.describe Book do
+  before(:all) do
+    ActiveRecord::Base.establish_connection(adapter: "postgresql", database: "rein_test")
+    CreateBooksTable.new.migrate(:up)
+  end
+
+  it "raises an error if the title is not present" do
+    expect { create_book(title: "") }.to raise_error(ActiveRecord::StatementInvalid, /PG::CheckViolation/)
+    expect { create_book(title: "The Origin of Species") }.to_not raise_error
+  end
+
+  it "raises an error if the state is invalid" do
+    expect { create_book(state: "burned") }.to raise_error(ActiveRecord::StatementInvalid, /PG::CheckViolation/)
+    expect { create_book(state: "on_hold") }.to_not raise_error
+  end
+
+  it "raises an error if the published month is not between 1 and 12" do
+    expect { create_book(published_month: 0) }.to raise_error(ActiveRecord::StatementInvalid, /PG::CheckViolation/)
+    expect { create_book(published_month: 13) }.to raise_error(ActiveRecord::StatementInvalid, /PG::CheckViolation/)
+    expect { create_book(published_month: 1) }.to_not raise_error
+  end
+end
