abf9e0879aaffb421c3a547b6e017846ed557427
Josh Bassett <josh.bassett@gmail.com>
2017-04-10 22:11:42 +1000

    Add reversible integration spec

---
+++ spec/integration/reversible_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+class TableConstraint < ActiveRecord::Base
+  self.table_name = "information_schema.table_constraints"
+end
+
+RSpec.describe "Reversible" do
+  before do
+    ActiveRecord::Migrator.migrate(MIGRATIONS_PATH)
+  end
+
+  it "reverses foreign key constraints" do
+    expect(TableConstraint.where(constraint_name: "books_author_id_fk")).to exist
+    ActiveRecord::Migrator.down(MIGRATIONS_PATH, 2)
+    expect(TableConstraint.where(constraint_name: "books_author_id_fk")).to_not exist
+  end
+
+  it "reverses inclusion constraints" do
+    expect(TableConstraint.where(constraint_name: "books_state")).to exist
+    ActiveRecord::Migrator.down(MIGRATIONS_PATH, 1)
+    expect(TableConstraint.where(constraint_name: "books_state")).to_not exist
+  end
+
+  it "reverses null constraints" do
+    expect(TableConstraint.where(constraint_name: "books_due_date")).to exist
+    ActiveRecord::Migrator.down(MIGRATIONS_PATH, 1)
+    expect(TableConstraint.where(constraint_name: "books_due_date")).to_not exist
+  end
+
+  it "reverses numericality constraints" do
+    expect(TableConstraint.where(constraint_name: "books_published_month")).to exist
+    ActiveRecord::Migrator.down(MIGRATIONS_PATH, 1)
+    expect(TableConstraint.where(constraint_name: "books_published_month")).to_not exist
+  end
+
+  it "reverses presence constraints" do
+    expect(TableConstraint.where(constraint_name: "books_title")).to exist
+    ActiveRecord::Migrator.down(MIGRATIONS_PATH, 1)
+    expect(TableConstraint.where(constraint_name: "books_title")).to_not exist
+  end
+end
